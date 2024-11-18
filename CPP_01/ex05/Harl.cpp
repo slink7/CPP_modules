f26cb5b87f31a3baea30294a84d9c3e5371717a4
Sebastien Cambier <slink794@gmail.com>
2024-11-18 17:28:42 +0100

    Added helpfull comments

--- CPP_01/ex05/Harl.cpp
+++ CPP_01/ex05/Harl.cpp
@@ -23,17 +23,17 @@ void Harl::complain(const std::string& level) const {
 }
 
 void Harl::debug(void) const {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !\n";
+	std::cout << "[DEBUG] I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !\n";
 }
 
 void Harl::info(void) const {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger ! If you did, I wouldn’t be asking for more !\n";
+	std::cout << "[INFO] I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger ! If you did, I wouldn’t be asking for more !\n";
 }
 
 void Harl::warning(void) const {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n";
+	std::cout << "[WARNING] I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n";
 }
 
 void Harl::error(void) const {
-	std::cout << "This is unacceptable ! I want to speak to the manager now.\n";
+	std::cout << "[ERROR] This is unacceptable ! I want to speak to the manager now.\n";
 }
