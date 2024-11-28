33e71d4e79f455873c795022fcbbf74af8c12b1b
scambier <scambier@student.42.fr>
2024-11-28 12:19:08 +0100

    Added extra tabs

--- CPP_02/ex02/Fixed.hpp
+++ CPP_02/ex02/Fixed.hpp
@@ -34,9 +34,9 @@ public:
 	float	toFloat(void) const;
 	int		toInt(void) const;
 
-	static Fixed	&min(Fixed &a, Fixed &b);
+	static Fixed		&min(Fixed &a, Fixed &b);
 	static const Fixed	&min(const Fixed &a, const Fixed &b);
-	static Fixed	&max(Fixed &a, Fixed &b);
+	static Fixed		&max(Fixed &a, Fixed &b);
 	static const Fixed	&max(const Fixed &a, const Fixed &b);
 	
 private:
