c75fb32df5ab4db4d620d2f3877ab8fee819dd29
Sebastien Cambier <scambier@student.42.fr>
2025-04-02 19:23:09 +0200

    Added missing canonical form for wrongcat

--- CPP_04/ex00/WrongCat.cpp
+++ CPP_04/ex00/WrongCat.cpp
@@ -5,8 +5,17 @@ WrongCat::WrongCat() {
 	std::cout << "Created WrongCat\n";
 }
 
+WrongCat::WrongCat(const WrongCat &cpy) {
+	*this = cpy;
+}
+
 WrongCat::~WrongCat() {
 	std::cout << "Destroyed WrongCat\n";
+}
+
+WrongCat &WrongCat::operator=(const WrongCat &cpy)
+{
+	type = cpy.type;
 }
 
 // void	WrongCat::makeSound() const {
