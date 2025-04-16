c2edd9c45453aaba3dc982e50bd94ed156e4ae9a
Sebastien Cambier <scambier@student.42.fr>
2025-04-16 15:47:24 +0200

    Made a better main

--- CPP_06/ex00/main.cpp
+++ CPP_06/ex00/main.cpp
@@ -4,8 +4,12 @@
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
-		std::cout << "Needs a scalar argument (1, 4.6f, 9.6, c, etc)\n";
+		std::cout << "\e[1;31mNeeds at least one scalar argument (1, 4.6f, 9.6, c, etc)\n\e[0m";
 		return (0);
 	}
-	ScalarConverter::convert(argv[1]);
+	for (int k = 1; k < argc; k++) {
+		std::cout << "\e[1;36m\n\tConversion for \e[1;32m\"" << argv[k] << "\"\e[1;36m :\n\n\e[0m";
+		ScalarConverter::convert(argv[k]);
+		std::cout << "\n";
+	}
 }
