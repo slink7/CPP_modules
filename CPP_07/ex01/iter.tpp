17d563d2c8ddda219a165812749a30efe2a94a15
Sebastien Cambier <scambier@student.42.fr>
2025-04-18 16:46:28 +0200

    Added ex01

---
+++ CPP_07/ex01/iter.tpp
@@ -0,0 +1,15 @@
+#ifndef ITER_TPP
+#define ITER_TPP
+
+template<class T>
+void	print(T& value) {
+	std::cout << value << "\n";
+}
+
+template<class T>
+void	iter(T* array, unsigned int array_length, void(*f)(T&)) {
+	for (unsigned int k = 0; k < array_length; ++k)
+		f(array[k]);
+}
+
+#endif
---
+++ CPP_07/ex01/main.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <string>
+
+#include "iter.tpp"
+
+void	double_it(int& value) {
+	value *= 2;
+}
+
+void	shout_now(std::string& str) {
+	for (unsigned long k = 0; k < str.length(); ++k) {
+		if (str[k] <= 'z' && str[k] >= 'a')
+			str[k] += 'A' - 'a';
+	}
+}
+
+int main() {
+	std::string strs[] = {
+		"String A",
+		"String B",
+		"String C",
+		"String D",
+		"String E",
+	};
+
+	int			integers[] = {
+		1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+	};
+
+	iter(integers, sizeof(integers) / sizeof(int), print);
+	std::cout << "Doubling it and giving it to the next function call!\n";
+	iter(integers, sizeof(integers) / sizeof(int), double_it);
+	iter(integers, sizeof(integers) / sizeof(int), print);
+	std::cout << "Now to strings, ...\n";
+	iter(strs, sizeof(strs) / sizeof(std::string), print);
+	std::cout << "I'm about to shout now !\n";
+	iter(strs, sizeof(strs) / sizeof(std::string), shout_now);
+	iter(strs, sizeof(strs) / sizeof(std::string), print);
+}
